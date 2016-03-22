6ef05a644ae266070d8adb23a05127b62e8a1422
Abe <abe.kazemzadeh@gmail.com>
2016-03-22 15:42:02 -0700

    fix error introduced during merge

--- stream_framework/tests/settings.py
+++ stream_framework/tests/settings.py
@@ -3,9 +3,7 @@ import os
 STREAM_DEFAULT_KEYSPACE = 'test_stream_framework'
 
 if os.environ.get('TEST_CASSANDRA_HOST'):
-
-
-stream_framework_CASSANDRA_HOSTS = "cass"#[os.environ['TEST_CASSANDRA_HOST']]
+    stream_framework_CASSANDRA_HOSTS = "cass"#[os.environ['TEST_CASSANDRA_HOST']]
 
 SECRET_KEY = 'ob_^kc#v536)v$x!h3*#xs6&l8&7#4cqi^rjhczu85l9txbz+W'
 STREAM_CASSANDRA_CONSITENCY_LEVEL = 'ONE'
